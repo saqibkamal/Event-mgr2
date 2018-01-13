1ee3377e841c54b0f7eaa3b69dd58feaa7667493
Saqib kamal <saqib.kamal01@gmail.com>
2018-01-14 01:11:34 +0530

    Final1

---
+++ app/src/main/java/kamal/saqib/eventmanager/PresenterActivity.java
@@ -0,0 +1,13 @@
+package kamal.saqib.eventmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PresenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presenter);
+    }
+}
